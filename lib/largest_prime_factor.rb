9c75e738d7c263b22a7cecb8ad33fc8ba7bfea75
jkhoang313 <jkhoang313@gmail.com>
2017-02-03 10:25:19 -0500

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,26 @@
 # Enter your procedural solution here!
+require 'pry'
+require 'prime'
+def largest_prime_factor(number)
+  highest_factor = number
+
+  (2..Math.sqrt(number)).each do |number|
+    while highest_factor % number == 0 && (highest_factor/number != 1)
+      highest_factor = highest_factor / number
+    end
+  end
+  highest_factor
+end
+
+# def largest_prime_factor(number)
+#   range = number/2
+#   if number.prime?
+#     return number
+#   else
+#     (3..range).to_a.reverse.each do |factor|
+#       if number % factor == 0 && factor.prime?
+#         return factor
+#       end
+#     end
+#   end
+# end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,14 @@
 # Enter your object-oriented solution here!
+require 'pry'
+class LargestPrimeFactor
+  attr_accessor :number
+
+  def initialize(number)
+    @number = number
+    (2..Math.sqrt(@number)).each do |num|
+      while @number % num == 0 && (@number/num != 1)
+        @number = @number / num
+      end
+    end
+  end
+end
